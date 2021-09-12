1dde6dfb48e0b0f79d2565eb247e7bc03debc6dc
Weslleyw10 <weslleylopes.dev@gmail.com>
2021-09-12 12:10:20 -0300

    gdrive-app finished

--- gdrive-webapp/public/app.js
+++ gdrive-webapp/public/app.js
@@ -1,1 +1,20 @@
-console.debug('hello world!')
+import AppController from './src/appController.js'
+import ConnectionManager from './src/connectionManager.js'
+import ViewManager from './src/viewManager.js'
+import DragAndDropManager from './src/dragAndDropManager.js'
+
+const API_URL = "https://127.0.0.1:3000"
+
+const appController = new AppController({
+    viewManager: new ViewManager(),
+    dragAndDropManager: new DragAndDropManager(),
+    connectionManager: new ConnectionManager({
+        apiUrl: API_URL
+    })
+})
+
+try {
+    await appController.initialize()
+} catch (error) {
+    console.error('error on initalizing', error)
+}
